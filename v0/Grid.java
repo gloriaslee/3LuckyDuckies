86b2bbf1f0a302f8814ecbd8274324ac74411192
zeeyingg <zjian30@stuy.edu>
2022-01-16 14:00:19 -0500

    grid

--- v0/Grid.java
+++ v0/Grid.java
@@ -3,17 +3,38 @@ import java.util.ArrayList;
 
 public class Grid{
 
-  public static int[][] gridGen(int rows, int column){
-    String[][] grid = new String[rows][column];
-    for (int row = 0; row < grid.length; row++){
-      for (int column = 0; column < grid[row].length; column){
-        grid[row][column] = "null";
+  int row = 0, column = 0;
+  String[][] grid = new String[row][column];
+
+  public Grid(){
+    System.out.println(grid);
+  }
+
+  public Grid (int rowsInput, int columnInput){
+    grid = new String[rowsInput][columnInput];
+    for (row = 0; row < grid.length; row++){
+      for (column = 0; column < grid[row].length; column++){
+          grid[row][column] = ". ";
       }
-    } return grid;
+    } System.out.println(grid);
+  }
+
+  public String toString(){
+    return this.grid;
   }
 
   public static void main(String[] args){
-    System.out.println(gridGen(3, 3));
+    Grid bob = new Grid();
+    Grid richard = new Grid(3, 3);
+    System.out.println(bob);
+    System.out.prinln(richard);
   }
 
 }
+
+// class Inner{
+//   public Inner{
+//
+//   }
+//
+// }
