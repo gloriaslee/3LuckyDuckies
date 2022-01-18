5ca3d329624ac4b6eea3656fbfd983192b2c1cad
zeeyingg <zjian30@stuy.edu>
2022-01-18 13:37:18 -0500

    v1

---
+++ v1/Grid.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Grid{
+
+  public int rows = 0;
+  public int columns = 0;
+  DotCluster[][] contents = new DotCluster[rows][columns];
+
+  public Grid(){
+    contents = new DotCluster[rows][columns];
+  }
+
+  public Grid (int rowsInput, int columnInput){
+    this();
+    //Grid output = new Grid();
+    rows = rowsInput;
+    columns = columnInput;
+    contents = new DotCluster[rowsInput][columnInput];
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < columns; j++){
+          contents[i][j] = new DotCluster(0, 34);
+          //changing maxDot for special DotClusters:
+          if(i==0||i==rows-1){
+            if(j==0||j==columns-1){
+              contents[i][j].maxDot=2;
+            }
+          }else if(i==0||i==rows-1||j==0||j==columns-1){
+            contents[i][j].maxDot = 3;
+          }else{
+            contents[i][j].maxDot = 4;
+          }
+      }
+    }
+  }
+
+  public void addDot(int colorIn, int x, int y){
+    // find the location of the dot that needs to be added
+    // just add one to that dotCluster
+    contents[x][y].numDots = contents[x][y].numDots + 1;
+    contents[x][y].color = colorIn;
+  }
+
+  // public String toString(){
+  //   String output = "";
+  //   for (DotCluster[] row: contents){
+  //     for (DotCluster i : row){
+  //       output += i.toString() + " ";
+  //     }
+  //     output += "\n";
+  //   }
+  //   return output;
+  // }
+
+  public String toString(){
+    String output = "  ";
+     // numbers at the top
+    for (int rw = 0; rw < columns; rw++){
+      output += rw%10 + " ";
+    }
+    output += "\n";
+    for (int j = 0; j < rows; j++){
+      output += j%10 + " ";
+      for (int i = 0; i < columns; i++){
+        output += contents[j][i].toString() + " ";
+      }
+      output += "\n";
+  } return output;
+  }
+
+}
---
+++ v1/Woo.java
@@ -0,0 +1,8 @@
+public class Woo {
+
+  public static void main(String[] args){
+    Game startGame = new Game();
+    startGame.turn();
+    }
+
+  }
