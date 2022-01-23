530af5ff81d7f979fa2927f02a3a6cd1320fa9f2
gloriaslee <glee20@stuy.edu>
2022-01-23 13:27:20 -0500

    text

--- v5/Game.java
+++ v5/Game.java
@@ -1,7 +1,12 @@
 import java.util.*;
+import java.lang.Integer;
 
 public class Game{
 
+  private static final int BRIGHT = 1;
+  private static final int DARK = 2;
+  private static final int ITALICS = 3;
+  private static final int BLACK = 30;
   private static final int RED = 31;
   private static final int GREEN = 32;
   private static final int YELLOW = 33;
@@ -9,30 +14,30 @@ public class Game{
   private static final int MAGENTA = 35;
   private static final int CYAN = 36;
   private static final int WHITE = 37;
-  public static final int RESET = 0;
-  public static final String CLEAR_SCREEN =  "\033[2J";
+  // private static final String RESET = color(40,37)+SHOW_CURSOR;
+  private static final int RESET = 0;
+  private static final String CLEAR_SCREEN =  "\033[2J";
+  private static final String HIDE_CURSOR =  "\033[?25l";
+  private static final String SHOW_CURSOR =  "\033[?25h";
 
   private int turnCounter = 1;
   private int redTurn = -1; // -1 means green turn, 1 means red turn
-  int rowInput=0;
-  int colInput=0;
-  String currentColor;
-  int colorInt;
-
-  //private static Scanner greenPlayer = new Scanner(System.in);
+  private int rowInput = 0;
+  private int colInput = 0;
+  private String currentColor;
+  private int colorInt;
   public Grid gameBoard = new Grid(9,6);
 
 
   public boolean turn() {
-    Text.clear();
-    //System.out.println(CLEAR_SCREEN);
+    System.out.println(Text.CLEAR_SCREEN);
     if(turnCounter == 1){
         System.out.println("Welcome to ... (drumroll please) CHAIN REACTION!!!");
     }
     System.out.println("This is Turn #" + turnCounter + "!");
     Scanner player = new Scanner(System.in);
       // check which player needs to go:
-      System.out.println(gameBoard);
+    System.out.println(gameBoard);
       if (redTurn == 1) {
         colorInt = RED;
         currentColor = DotCluster.colorText(colorInt) + "RED" + DotCluster.colorText(RESET);
@@ -41,15 +46,18 @@ public class Game{
         currentColor = DotCluster.colorText(colorInt) + "GREEN" + DotCluster.colorText(RESET);
       }
 
-      System.out.println(currentColor + " Player: Please input a location at which you would like to add a dot.");
-      System.out.println("First input desired row, then desired column");
-
+      System.out.println("* " + currentColor + " Player: Please input a location at which you would like to add a dot."
+      +"\n* " + "First input desired row (vertical), then desired column (horizontal)"
+      +"\n* " + "If you would like to reset your input, type RESET."
+      +"\n* You can only add dots to an empty box or a box with dots of the same color."
+      +"/n* Boxes will explode ");
 
       while(true){
         try{
 //for when the user inputs non integer input
           rowInput = player.nextInt();
           colInput = player.nextInt();
+          Integer colInteger = colInput;
           boolean shouldBreak = true;
 //for when index input will be out of bounds
           if(rowInput <0 || rowInput>=gameBoard.rows||colInput <0 || colInput>=gameBoard.columns)
@@ -59,14 +67,14 @@ public class Game{
       }
 // accounts for dots inserted in the wrong color and not inserted in blue
       if(gameBoard.contents[rowInput][colInput].color != colorInt && gameBoard.contents[rowInput][colInput].color != BLUE){
-            System.out.println("Try again. You put a dot somewhere your opponent has already claimed." + "\n");
+            System.out.println("You put a dot somewhere your opponent has already claimed." + "\n");
             shouldBreak = false;
       }
       if(shouldBreak) {
         break;
       }
     } catch(Exception e){
-          System.out.println("Invalid input. Try again");
+          System.out.println("Re-enter your coordinates.");
           player.nextLine();
         }
     }
@@ -85,11 +93,5 @@ public class Game{
     }
     System.out.println(gameBoard);
   }
-
-
-
-
-
-
 
 }
