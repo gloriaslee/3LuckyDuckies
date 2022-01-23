676b43d60fecf6fb877f6d6f8112d2087fd0d791
gloriaslee <glee20@stuy.edu>
2022-01-23 12:13:15 -0500

    scanner

--- v5/Game.java
+++ v5/Game.java
@@ -10,8 +10,8 @@ public class Game{
   private static final int CYAN = 36;
   private static final int WHITE = 37;
   public static final int RESET = 0;
-  //public static final String CLEAR_SCREEN =  "\033[2J";
-
+  public static final String CLEAR_SCREEN =  "\033[2J";
+  private int turnCounter = 1;
   private int redTurn = -1; // -1 means green turn, 1 means red turn
 
   //private static Scanner greenPlayer = new Scanner(System.in);
@@ -21,10 +21,12 @@ public class Game{
   public boolean turn(){
     // will be used to alternate between player turns
     // take input on dot location
-    //System.out.println(CLEAR_SCREEN+RESET);
+    System.out.println(CLEAR_SCREEN);
+    System.out.println("This is Turn #"+turnCounter+"!");
   Scanner player = new Scanner(System.in);
     int rowInput=0;
     int colInput=0;
+
     String currentColor;
     int colorInt;
 
@@ -52,56 +54,50 @@ public class Game{
       //   return false;
       //
       // // }
-
+      boolean shouldBreak = true;
       while(true){
         try{ //for when the user inputs non integer input
 
           rowInput = player.nextInt();
           colInput = player.nextInt();
-player.nextLine();
-          break;
+
+          if(rowInput <0 || rowInput>=gameBoard.rows||colInput <0 || colInput>=gameBoard.columns)
+          { //for when index input will be out of bounds
+              System.out.println("Try again. Index input out of bounds of grid size\n");
+
+          shouldBreak = false;
+            }
+
+      if(gameBoard.contents[rowInput][colInput].color != colorInt && gameBoard.contents[rowInput][colInput].color != BLUE){
+                // accounts for dots inserted in the wrong color and not inserted in blue
+              System.out.println("Try again. You put a dot somewhere your opponent has already claimed." + "\n");
+
+            //wait(3000);
+
+            shouldBreak = false;
+      }
+
+      if(shouldBreak){
+        break;
+      }
+
         } catch(Exception e){
           System.out.println("Invalid input. Try again");
           player.nextLine();
-          turn();
+
         }
       }
 
-      if(rowInput <0 || rowInput>=gameBoard.rows||colInput <0 || colInput>=gameBoard.columns)
-    { //for when index input will be out of bounds
-        System.out.println("Try again. Index input out of bounds of grid size\n");
-
-        turn();
+  
 
 
-      }
-      if (gameBoard.contents[rowInput][colInput].color != colorInt && gameBoard.contents[rowInput][colInput].color != BLUE){
-            // accounts for dots inserted in the wrong color and not inserted in blue
-          System.out.println("Try again. You put a dot somewhere your opponent has already claimed." + "\n");
-
-            //wait(3000);
-          turn();
-
-        //System.out.println("You put in the following y coordinate: " + rowInput + " and the following x coordinate: " + colInput);
-      }
       gameBoard.addDot(colorInt, rowInput, colInput);
+      System.out.println("You put in the following y coordinate: " + rowInput + " and the following x coordinate: " + colInput);
+      System.out.println("Dot added!");
       redTurn *= -1;
+      turnCounter +=1;
       return true;
   } // end method
-//
-// public void inputScan(){
-//   while(true){
-//     try{
-//       System.out.println("Trying:");
-//         rowInput = player.nextInt();
-//         colInput = player.nextInt();
-//         break;
-//       } catch(Exception e){
-//         System.out.println("Invalid input. Try again");
-//
-//       }
-//   }
-// }
 
 
   public void runGame() {
